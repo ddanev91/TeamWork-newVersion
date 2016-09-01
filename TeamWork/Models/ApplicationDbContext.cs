97191d1e0b9d8a8da4195030f9c6c5b3fc3c2224
danev <didanev91@abv.bg>
2016-09-01 21:09:49 +0300

    up

---
+++ TeamWork/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TeamWork.Models
+{
+   
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public System.Data.Entity.DbSet<TeamWork.Models.Post> Posts { get; set; }
+
+        public System.Data.Entity.DbSet<TeamWork.Models.Gallery> Galleries { get; set; }
+    }
+}
---
+++ TeamWork/Models/Gallery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeamWork.Models
+{
+    public class Gallery
+    {
+        public Gallery()
+        {
+            this.Date = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+
+        [Required]
+        public DateTime Date { get; set; }
+
+       
+    }
+}
